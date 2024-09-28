8a33858ddf3ec3cb912579c7909de130417ff18c
Pushkraj P Naik <pushkrajpnaik@users.noreply.github.com>
2024-09-29 01:18:11 +0530

    data types in cpp

---
+++ datatypes.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //int
+    int x;
+    cin>>x;
+    //long
+    long y;
+    cin>>y;
+    //long long is basically even longer
+    //float- includes decimal numebers as well
+    float z;
+    cin>>z;
+    cin>>y;
+    cout<<"value of x is:"<<x<<endl;
+    cout<<"value of y is:"<<y<<endl;
+    cout<<"value of z is:"<<z<<endl;
+   return 0;
+}
