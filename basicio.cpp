e912d88c947a269a62444fd538a02418068f3819
Pushkraj P Naik <pushkrajpnaik@users.noreply.github.com>
2024-09-29 00:58:14 +0530

    Basic I/O operations

---
+++ basicio.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string x;
+    int y;
+    cout<<"Enter your name:";
+    cin>>x;
+    cout<<"Enter your age:";
+    cin>>y;
+    cout<<"\nYour name is :"<<x<<"\nYour age is:"<<y<<endl;
+    return 0;
+}
