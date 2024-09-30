4e643c7b533c61f0e18f1a8f2b094998b7d10e02
Pushkraj P Naik <pushkrajpnaik@users.noreply.github.com>
2024-09-30 11:23:55 +0530

    In this, I've done array creation of different dimensions

---
+++ arrays.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //1D array
+    //can be any datatype but should be constanf ro all elements in the array
+    int arr[5];
+    cout<<"Enter the values of the arrays:\n";
+    cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+    cout<<arr[4];
+    return 0;
+}
