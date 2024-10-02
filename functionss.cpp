b250a65758b986ef19f20ec511b95e0879dbb7bc
Pushkraj P Naik <pushkrajpnaik@users.noreply.github.com>
2024-10-03 00:58:48 +0530

    functions with different parameters and return types

---
+++ functionss.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+//functions are set of codes which perform something and can be used multiple times. They are used to modularize codes and increase their readability
+   //return types can be void(no return values) int etc
+   //can have parameters or be non parameterised
+// void printName(string name){
+//     //declaration oo]f taking input can be donen\ outside the function as well but youll need to give parameter -> void printName(string name){}
+//     // string name;
+//     // cout<<"enter your name: "<<endl;
+//     // cin>>name;
+//     // cout<<"hello "<<name<<"!"<<endl;
+
+//     //without declaring inside the function
+//     cout<<"hello "<<name<<endl;
+// }
+// int main()
+// {
+//     // printName();
+//     //without declaring in the function well need to declare it in main and also give oarameter inside the function itself
+//     string name;
+//     cin>>name;
+//     printName(name);
+//     return 0; 
+// }
+
+
+
+
+/*funtction to take two nos and print their sum*/
+// void sum(float n1,float n2){
+//     cout<<"the sum of the numbers is :"<<n1+n2<<endl;
+// }
+//can use int or float as return type instead
+float sum(float n1,float n2){
+    float n3 = n1+ n2;
+    return n3;
+}
+int main(){
+    float n1,n2;
+    cout<<"Enter 1st number: ";
+    cin>>n1;
+    cout<<"Enter 2nd number: ";
+    cin>>n2;
+    //this only does sum but does not return it sum(n1,n2); ->used when return type is void
+    //for non void return type
+    float add = sum(n1,n2);
+    cout<<"The sum is: "<<add;
+}
