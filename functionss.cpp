c745fcfd485c96357bf797e3e063a652099f9388
Pushkraj P Naik <pushkrajpnaik@users.noreply.github.com>
2024-10-03 01:07:18 +0530

    functioons with different return types and parameters

--- functionss.cpp
+++ functionss.cpp
@@ -26,23 +26,40 @@ using namespace std;
 
 
 
+
 /*funtction to take two nos and print their sum*/
 // void sum(float n1,float n2){
 //     cout<<"the sum of the numbers is :"<<n1+n2<<endl;
 // }
 //can use int or float as return type instead
-float sum(float n1,float n2){
-    float n3 = n1+ n2;
-    return n3;
+// float sum(float n1,float n2){
+//     float n3 = n1+ n2;
+//     return n3;
+// }
+// int main(){
+//     float n1,n2;
+//     cout<<"Enter 1st number: ";
+//     cin>>n1;
+//     cout<<"Enter 2nd number: ";
+//     cin>>n2;
+//     //this only does sum but does not return it sum(n1,n2); ->used when return type is void
+//     //for non void return type
+//     float add = sum(n1,n2);
+//     cout<<"The sum is: "<<add;
+// }
+
+
+//To find minimum or maximum math.h has in built functions but we can make out own if we wanted to
+int maxx(int n1,int n2){
+    if(n1>=n2){
+        return n1;
+    }
+    else return n2;
 }
 int main(){
-    float n1,n2;
-    cout<<"Enter 1st number: ";
-    cin>>n1;
-    cout<<"Enter 2nd number: ";
-    cin>>n2;
-    //this only does sum but does not return it sum(n1,n2); ->used when return type is void
-    //for non void return type
-    float add = sum(n1,n2);
-    cout<<"The sum is: "<<add;
+    int n1,n2;
+    cin>>n1>>n2;
+    int maximum = maxx(n1,n2);
+    cout<<"the maximum between both these numbers is: "<<maximum;
+    return 0;
 }
