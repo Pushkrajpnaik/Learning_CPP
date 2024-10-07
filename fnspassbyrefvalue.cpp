c01022a288e199ea47c5cdaf87e1718980eb7f2a
Pushkraj P Naik <pushkrajpnaik@users.noreply.github.com>
2024-10-07 23:21:19 +0530

    usage of pass by reference and value

--- fnspassbyrefvalue.cpp
+++ fnspassbyrefvalue.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
 using namespace std;
+//in pass by reference we use pointers whereas in pass by value we directly pass the value
 
+
+//pass by value
+void doSomething(int num){
+   cout<<num<<endl;
+   num+=5;
+   cout<<num<<endl;
+   num+=5;
+   cout<<num<<endl;
+}
 int main()
-{
-    
+{ 
+   int num = 10;
+   cin>>num;
+   doSomething(num);
+   //op is 10 for this as in the function we pass only a copy of the value and are not doing anything with thte original value of the number
+   cout<<num<<endl;
    return 0;
 }
